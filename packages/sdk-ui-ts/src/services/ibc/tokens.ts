0bed2f1c44434930cba7d22e5fa9ed0deb2ea51b
shane-moore <skm1790@gmail.com>
2023-10-16 11:32:01 -0700

    chore: test 7

--- packages/sdk-ui-ts/src/services/ibc/tokens.ts
+++ packages/sdk-ui-ts/src/services/ibc/tokens.ts
@@ -36,7 +36,7 @@ function ibcTokenMetadataToToken(
       tokenVerification: TokenVerification.External,
       ibc: {
         hash: (token.contractAddr || '').replace('ibc/', ''),
-        path: 'squirel',
+        path: 'squirelz',
         channelId: '',
         decimals: token.decimals || 18,
         symbol: token.symbol || 'Unknown',
